7a5ec2161813922a36fb98bc0f03aaed10ff3901
HemantRajpal-9018 <71565751+HemantRajpal-9018@users.noreply.github.com>
2022-12-06 03:32:25 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+      <!-- Step 3 must use the component-->
+      <HeaderTop bookName="Laptop Giant" 
+       authorName="Laptop Models"/>
+      <BookBox :books="books"/>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+// Step 1: Import components
+import HeaderTop from './components/HeaderTop.vue'
+import BookBox from './components/BookBox.vue'
+
+
+
+export default{
+    name: 'App',
+
+    // step 2: must register the component
+    components: {
+    HeaderTop,
+    BookBox
+},
+    data(){
+      return{
+        books:[]
+      }
+    },
+      methods:{
+        //promiss 
+          async fetchLaptop(){
+          const res = await fetch ("https://laptop-giant-app.herokuapp.com/api")
+          const data = await res.json()
+          console.log(data)
+
+          return data.Laptop
+        }
+      },
+      async created(){
+        this.books = await this.fetchLaptop()
+      }
   }
-}
+
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+*{
+box-sizing: border-box;
+margin: 0;
+padding: 0;
+}
+body{
+font-family:  'Monsterrat', sans-serif;
+}
+.container{
+max-width: 100%;
+min-height: 300px;
+overflow:  auto;
+margin: 30px auto;
+border: 0.3 em solid black;
+border-radius: 5px
+}
+
+div{
+margin-bottom: 0.5em;
 }
 </style>
---
+++ src/components/BookItem.vue
@@ -0,0 +1,53 @@
+<template>
+        <div>
+
+            <h2>{{book.Brand}}</h2>
+            <h4>{{book.Operating_System}}</h4>
+            <h4>{{book.Processor}}</h4>
+            <h4>{{book.Memory}}</h4>
+            <h4>{{book.Storage_Size}}</h4>
+            <h4>{{book.Release_Year}}</h4>
+            <h4>{{book.Model_Number}}</h4>
+            
+
+
+        </div>
+
+</template>
+
+<script>
+
+    export default{
+        name: 'BookItem',
+        props: {
+            book: Array
+        }
+    }
+    
+
+</script>
+
+
+<style scoped>
+
+
+h4{
+    margin: 2px;
+    color:#ff4500;
+}
+h2{
+    color:red;
+}
+div{
+     background-color: #7CF3A0;
+     color:#81B622;
+     color: black;
+     padding: 1em;
+     margin: 5px;   
+     border-radius: 30px;
+    }
+
+
+
+
+</style>
